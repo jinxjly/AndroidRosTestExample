00c7917a114ab0515100ab82e9301b4c065b51f4
jinxy <jinjoo@media.mit.edu>
2014-10-30 17:17:42 -0700

    now displaying to textview

--- src/com/example/rosapptest/MainActivity.java
+++ src/com/example/rosapptest/MainActivity.java
@@ -1,23 +1,53 @@
 package com.example.rosapptest;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.app.Activity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    public ROSComm comm;
+	public static final long TIME_DELAY_MS = 100; 
 
+    public ROSComm comm;
+    public Handler handler;
+    
+    public TextView displayText;
+    public Runnable updateTextRunnable;
+		 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-					
+		displayText = (TextView)findViewById(R.id.displayText);
+		
         //Set up ROS communication
         comm = new ROSComm();
-		ROSNodeMaker.runROSNode(comm, ROSComm.nodeName);	
-	}
+		ROSNodeMaker.runROSNode(comm, ROSComm.nodeName);
+		
+		//Set up Handler
+		handler = new Handler();
+
+		//Kind of a hacky way to update the textview periodically..
+		updateTextRunnable = new Runnable(){  
+			  public void run() {  				  
+				  if(comm.getSubscriberTest()!=null){
+					  if(comm.getSubscriberTest().hasData()){
+						  String msg = comm.getSubscriberTest().fetchData();
+						  displayText.setText(msg);
+					  }
+				  }
+			      handler.postAtTime(updateTextRunnable, SystemClock.uptimeMillis() + TIME_DELAY_MS);
+			     }  
+			 };  
+			 
+		//Start 		
+		handler.postAtTime(updateTextRunnable, SystemClock.uptimeMillis() + TIME_DELAY_MS);
+
+	}		
 
 	public void sendROSMessage(View view){
     	EditText editText = (EditText) findViewById(R.id.edit_message);
--- src/com/example/rosapptest/SubscriberTest.java
+++ src/com/example/rosapptest/SubscriberTest.java
@@ -6,16 +6,28 @@ import org.ros.node.topic.Subscriber;
 
 public class SubscriberTest implements MessageListener<std_msgs.String> {
 	protected Subscriber<std_msgs.String> subscriber;
+	protected String data = null;
 
 	public SubscriberTest(final ConnectedNode connectedNode) {
 		subscriber = connectedNode.newSubscriber("androidRos_test_subscriber", std_msgs.String._TYPE);
 		subscriber.addMessageListener(this);
 	}
 	
-	public void onNewMessage(std_msgs.String message) {
-		String data = message.getData();
-		if(data != null){
+	public void onNewMessage(std_msgs.String message) {		
+		data = message.getData();
+//		if(data!=null){
 			System.out.println(data);
-		}
+//		}
+	}
+	
+	public boolean hasData(){
+		return (data!=null)? true : false;
+	}
+	
+	public String fetchData(){
+		String temp = data;
+		this.data = null;
+		return temp;
 	}
 }
+
